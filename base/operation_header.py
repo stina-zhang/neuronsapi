2ec454ae5664b83a3ea6767cbf82672fd3fbc92a
stinazhang <jinji.zhang@domi100.com>
2020-04-08 13:41:33 +0800

    add get_case_depend add get_date_depend add get_field_depend

--- base/operation_header.py
+++ base/operation_header.py
@@ -5,13 +5,17 @@ import json
 
 class OperationHeader:
 
-    def get_token(self):
+    def login(self):
         url = "http://api.domi100.net/account/login"
         data = {"username": "13800138002", "password": "123456", "appId": "1469503131678220288"}
         header = {"Content-Type": "application/json"}
         rep = requests.post(url=url, data=json.dumps(data), headers=header)
-        token = rep.json()['result']['token']
+        return rep
+
+    def get_token(self):
+        token = self.login().json()['result']['token']
         return token
+
     def get_header(self):
         newToken = self.get_token()
         header = {"Content-Type": "application/json", "token": newToken}
